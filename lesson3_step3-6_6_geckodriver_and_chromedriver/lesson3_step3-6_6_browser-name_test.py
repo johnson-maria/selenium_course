cd46226e01b3fea643187ee2d31fb6d8ed3a9233
Maria Goriunova <aioa.account@gmail.com>
2024-01-05 16:34:14 +0500

    Update requirements and change function for finding elements

--- lesson3_step3-6_6_geckodriver_and_chromedriver/lesson3_step3-6_6_browser-name_test.py
+++ lesson3_step3-6_6_geckodriver_and_chromedriver/lesson3_step3-6_6_browser-name_test.py
@@ -1,9 +1,11 @@
+from selenium.webdriver.common.by import By
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
 
     # (selenium_env) D:\Projects\selenium_course\lesson3_step3-6_6_geckodriver_and_chromedriver>pytest -s -v --browser_name=firefox lesson3_step3-6_6_browser-name_test.py
     # или: pytest -s -v --browser_name=chrome lesson3_step3-6_6_browser-name_test.py
